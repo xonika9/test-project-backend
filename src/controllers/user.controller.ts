d974361428e22fe09b95a2d1b9183de8036bb310
xonika9 <xonika9@gmail.com>
2025-02-21 13:32:52 +0300

    add prettier, fix errors

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,15 +2,15 @@ import { Request, Response } from 'express';
 import * as userService from '../services/user.service';
 
 export const getProfile = async (req: Request & { user: { id: number } }, res: Response) => {
-  try {
-    const userId = req.user.id;
-    const user = await userService.getUserById(userId);
-    res.json(user);
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      res.status(500).json({ message: error.message });
-    } else {
-      res.status(500).json({ message: 'An unknown error occurred' });
+    try {
+        const userId = req.user.id;
+        const user = await userService.getUserById(userId);
+        res.json(user);
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            res.status(500).json({ message: error.message });
+        } else {
+            res.status(500).json({ message: 'An unknown error occurred' });
+        }
     }
-  }
 };
--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,19 +1,23 @@
-import { Request, Response, NextFunction } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { verifyToken } from '../utils/jwt';
 
-export const authenticate = (req: Request & { user?: { id: number } }, res: Response, next: NextFunction) => {
-  const token = req.headers.authorization?.split(' ')[1];
-  if (!token) return res.status(401).json({ message: 'Unauthorized' });
+export const authenticate = (
+    req: Request & { user?: { id: number } },
+    res: Response,
+    next: NextFunction,
+) => {
+    const token = req.headers.authorization?.split(' ')[1];
+    if (!token) return res.status(401).json({ message: 'Unauthorized' });
 
-  try {
-    const decoded = verifyToken(token);
-    req.user = { id: decoded.userId };
-    next();
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      res.status(401).json({ message: error.message });
-    } else {
-      res.status(401).json({ message: 'Invalid token' });
+    try {
+        const decoded = verifyToken(token);
+        req.user = { id: decoded.userId };
+        next();
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            res.status(401).json({ message: error.message });
+        } else {
+            res.status(401).json({ message: 'Invalid token' });
+        }
     }
-  }
 };
